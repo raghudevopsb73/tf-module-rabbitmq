df62b7cb42836221e451481b6d4faabf281669bd
Raghu K <raghuk.vit@gmail.com>
2023-07-03 08:40:09 +0530

    Update tf-module-rabbitmq/

--- iam.tf
+++ iam.tf
@@ -13,9 +13,13 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
-          "ssm:GetParameter"
+          "ssm:GetParameter",
+          "kms:Decrypt"
         ],
-        "Resource" : "arn:aws:ssm:us-east-1:739561048503:parameter/roboshop.${var.env}.${var.component}.*"
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:739561048503:parameter/roboshop.${var.env}.${var.component}.*",
+          var.kms_key_arn
+        ]
       }
     ]
   })
